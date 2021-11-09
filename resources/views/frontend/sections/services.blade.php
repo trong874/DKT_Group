29cfc43620619f8cd573e6ba52015929d811a136
tutepp <tutepp>
2021-11-09 15:00:25 +0700

    fix icon to image

--- resources/views/frontend/sections/services.blade.php
+++ resources/views/frontend/sections/services.blade.php
@@ -13,7 +13,7 @@
                         <div class="col-lg-4 col-md-6 swiper-slide" data-aos="fade-up" data-aos-delay="{{$key *= 100}}">
                             <div class="service-box blue">
                                 <div class="box">
-                                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Facebook_logo_36x36.svg/2048px-Facebook_logo_36x36.svg.png"/>
+                                    <img src="{{$item->image}}"/>
                                     <h3>{{$item->title}}</h3>
                                     <p>{!! $item->description !!}</p>
                                     {{--                    <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>--}}
