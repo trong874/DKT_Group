153a485fe32bf4fc6cf1d762a437a77a50cca16d
tutepp <tutepp>
2021-11-15 14:57:22 +0700

    tu update

--- resources/views/frontend/blog-single.blade.php
+++ resources/views/frontend/blog-single.blade.php
@@ -6,7 +6,7 @@
             <ol>
                 <li><a href="/">{{__('Trang chủ')}}</a></li>
                 <li><a href="/tin-tuc">{{__('Tin tức')}}</a></li>
-                <li><span>{{$news->title}}</span></li>
+                <li><span>{{$news[0]->title}}</span></li>
             </ol>
             <h2>{{__($page_title)??'DKT'}}</h2>
         </div>
@@ -20,20 +20,20 @@
 {{--                            <img src="{{$news[0]->image}}" alt="" class="img-fluid">--}}
 {{--                        </div>--}}
                         <h2 class="entry-title">
-                            <a href="{{$news->url}}">{{$news->title}}</a>
+                            <a href="{{$news[0]->url}}">{{$news[0]->title}}</a>
                         </h2>
 
                         <div class="entry-meta">
                             <ul>
-                                <li class="d-flex align-items-center"><i class="bi bi-person"></i>{{$news->user->name}}<a href="#"></a></li>
-                                <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="#"><time datetime="2020-01-01">{{$news->created_at}}</time></a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-person"></i>{{$news[0]->user->name}}<a href="#"></a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="#"><time datetime="2020-01-01">{{$news[0]->created_at}}</time></a></li>
                             </ul>
                         </div>
                         <div class="entry-content">
-                            <p>{!! $news->description !!}</p>
+                            <p>{!! $news[0]->description !!}</p>
                         </div>
-                        {!! $news->content !!}
-                        <span><i class="bi bi-eye-fill"></i>View: {{$news->totalviews}}</span>
+                        {!! $news[0]->content !!}
+                        <span><i class="bi bi-eye-fill"></i>View: {{$news[0]->totalviews}}</span>
                     </article>
                     <style>
                         article img {
@@ -60,7 +60,7 @@
         window.onload = function() {
             $.ajax({
                 method: "PUT",
-                url: "{{route('items.add-view',$news->id)}}",
+                url: "{{route('items.add-view',$news[0]->id)}}",
                 data: {
                     "_token": "{{ csrf_token() }}",
                 }
